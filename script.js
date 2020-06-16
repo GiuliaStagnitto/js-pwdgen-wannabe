4fbbe1db7ab76ce15bb55055a8890cb97b518800
Giux <giuliamelody@gmail.com>
2020-06-16 15:07:03 +0200

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+//Chiedi all’utente il suo nome
+var nome = prompt ("Inserisci il tuo nome: ");
+//Chiedi all’utente il suo cognome
+var cognome = prompt ("Inserisci il tuo cognome: ");
+//Chiedi all’utente il suo colore preferito
+var colore = prompt ("Inserisci il tuo colore preferito: ");
+
+//risultato
+document.getElementById("yes").innerHTML = nome + cognome + colore + 19;
